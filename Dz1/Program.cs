39b4c3ae773be6b973e1bd95e19382e239bd59e2
Serg-101 <golikov1979@gmail.com>
2022-05-30 01:05:25 +0300

    initial commit

---
+++ Dz1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+int[] GetNumbers() //вводим целолые числа с консоли через пробел и парсим строку с преобразованием в int 
+{
+    string[] numbers = Console.ReadLine().Split(' ');
+    int[] value = new int[numbers.Length];
+    int i = 0;
+    foreach (var item in numbers) value[i++] = int.Parse(item);
+    return value;
+}
+
+int GetPositiveNumbers(int[] numbers)  // находим положительные числа среди представленных
+{
+    int countPositiveNumbers = 0;
+    foreach (var item in numbers) if (item > 0) countPositiveNumbers++;
+    return countPositiveNumbers;
+}
+
+void PrintResult(int countPositiveNumbers) //  выводим результат в правильном виде
+{
+    if (countPositiveNumbers == 0)
+        System.Console.WriteLine("положительные числа не найдены");
+    else if (countPositiveNumbers == 1)
+        System.Console.WriteLine("Найдено одно число");
+    else if (countPositiveNumbers > 1 && countPositiveNumbers < 5)
+        System.Console.WriteLine($"Найдено {countPositiveNumbers} положительных числа среди введенных ранее.");
+    else 
+        System.Console.WriteLine($"Найдено {countPositiveNumbers} положительных чисел среди введенных ранее.");
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите через пробел несколько чисел, что бы подсчитать не  отрицательных чисел");
+System.Console.WriteLine("Например  0 7 8 -2 -2");
+int[] numbers = GetNumbers();
+int countPositiveNumbers = GetPositiveNumbers(numbers);
+PrintResult(countPositiveNumbers);
---
+++ Dz2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 43. Напишите программу, 
+которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+вероятно опечатка в задании.
+*/
+
+float GetNumber(string message) //ввод целолго числа с консоли с проверкой
+{
+    System.Console.Write(message);
+    while (true)
+        if (float.TryParse(Console.ReadLine(), out float result))
+            return result;
+        else
+            System.Console.WriteLine("Ошибка,  введите число");
+}
+
+float GetCoordinateX(float b1, float k1, float b2, float k2) // вычесление координаты x через систему уравнений
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+float GetCoordinateY(float coordinateX, float b1, float k1) // подставляем координату x и вычисляем y
+{
+    return (k1 * coordinateX + b1);
+}
+
+Console.Clear();
+System.Console.WriteLine("Найти точку пересечения двух прямых заданных формулой y=kx+b");
+float b1 = GetNumber("Введите b1 = ");
+float k1 = GetNumber("Введите k1 = ");
+float b2 = GetNumber("Введите b2 = ");
+float k2 = GetNumber("Введите k2 = ");
+float coordinateX = GetCoordinateX(b1, k1, b2, k2);
+float coordinateY = GetCoordinateY(coordinateX, b1, k1);
+System.Console.WriteLine($"Координаты пересечения (X={coordinateX}; Y={coordinateY})");
+
+/*
+а могло бы быть короче
+float x = (b2 - b1) / (k1 - k2);
+float y = (k1 * x + b1);
+Console.WriteLine($"Координаты пересечения (X={x}; Y={y})");
+/*
